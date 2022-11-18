3042c68eacefefc37f1b6fee5d247264c0487447
Diego-Sabino <dbrgamei5@gmail.com>
2022-11-18 11:06:01 -0300

    50% atividade 1

---
+++ AluraCS1/Data/MovieContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace AluraCS1.Data
+{
+	public class MovieContext : DbContext
+	{
+		public MovieContext(DbContextOptions<MovieContext> opt) : base (opt)
+		{
+
+		}
+	}
+}
+
