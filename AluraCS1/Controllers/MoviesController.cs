4e946958e96dd8c6ec9e84cb663cf4ad6a355c21
Diego-Sabino <dbrgamei5@gmail.com>
2022-11-18 09:35:59 -0300

    ingles

---
+++ AluraCS1/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MoviesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+
+	public class FilmeController : ControllerBase
+	{
+		private static List<Movie> movies = new List<Movie>();
+
+		[HttpPost]
+
+		public void CreateMovie([FromBody] Movie movie)
+		{
+            movie.id += 1;
+            movies.Add(movie);
+        }
+        [HttpGet]
+
+		public IEnumerable<Movie> GetAllMovies()
+		{
+			return movies;
+		}
+
+		public Movie GetMovieById()
+		{
+
+		}
+	}
+}
+
--- AluraCS1/Models/Movie.cs
+++ AluraCS1/Models/Movie.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace FilmesAPI.Models
+namespace MoviesAPI.Models
 {
-    public class Filme
+    public class Movie
     {
         public int id { get; internal set; }
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
-        public string Diretor { get; set; }
+        public string Director { get; set; }
         [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
         [Required(ErrorMessage = "O campo genero é obrigatório")]
-        public string Genero { get; set; }
+        public string Genres { get; set; }
         [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
-        public int Duracao { get; set; }
+        public int Duration { get; set; }
     }
 }
 
