68e7482b32cd5789e11e2963f14a94959616cfb0
Diego-Sabino <dbrgamei5@gmail.com>
2022-11-18 10:52:43 -0300

    atividade 3

--- AluraCS1/Controllers/MoviesController.cs
+++ AluraCS1/Controllers/MoviesController.cs
@@ -10,27 +10,35 @@ namespace MoviesAPI.Controllers
 	public class FilmeController : ControllerBase
 	{
 		private static List<Movie> movies = new List<Movie>();
+		private static int id = 1;	
 
 		[HttpPost]
 
-		public void CreateMovie([FromBody] Movie movie)
+		public IActionResult CreateMovie([FromBody] Movie movie)
 		{
-            movie.id += 1;
+            movie.id = id ++;
             movies.Add(movie);
+			return CreatedAtAction(nameof(GetMovieById), new { Id = movie.id }, movie);
         }
 
         [HttpGet]
 
-		public IEnumerable<Movie> GetAllMovies()
+		public IActionResult GetAllMovies()
 		{
-			return movies;
+			return Ok(movies);
 		}
 
         [HttpGet("{id}")]
 
-        public Movie GetMovieById(int id)
+        public IActionResult GetMovieById(int id)
 		{
-			return movies.FirstOrDefault(movie => movie.id == id);
+			Movie movie = movies.FirstOrDefault(movie => movie.id == id);
+
+			if (movie != null)
+			{
+				return Ok(movie);
+			}
+			return NotFound();
 		}
 	}
 }
